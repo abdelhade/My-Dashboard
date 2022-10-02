410216276320cffbb2d6b3b516de92d7b3da9a86
abdelhade <40345374+abdelhade@users.noreply.github.com>
2022-10-02 19:35:49 +0200

    Add files via upload

---
+++ test/main.js
@@ -0,0 +1,21 @@
+const name = "lidia";
+
+// Have const or let or var operator [ delete can't delete property ]
+
+// Can't = faluse
+// Can't Delete here = false
+// if the variable like name = "lidia" Here We Can Delete it { Answer = true  }
+
+let age = 21;
+
+// Here Don't have Const or let or var can delete it 
+
+// can = true
+// can delete here = true
+// if variable like let age = 21 Here We Can't delted it { Answer = false }
+
+console.log(delete name); // Answer = False
+console.log(delete age); // Answer = true
+
+
+Correct Answer Is [ A ] = [ false ,,, true    ]
